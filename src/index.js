6478fb34ffdcc1733f0a192b0547485e193ea4c0
Danko Karleusa <dankomatichevich@gmail.com>
2022-03-04 10:32:47 -0800

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-// write your code here
+const flatGramAPI = ' http://localhost:3000/images/1'
+//currentImage = document.querySelector(".image")
+//let displayImage = document.createElement('img');
+let displayTitle;
+let displayLikes;
+let displayCommments;
+
+fetch(flatGramAPI)
+  .then((res) => res.json())
+  .then(displayImage)
+  .catch(console.err)
+
+//   function displayImage(displayObject) {
+//     // displayObject.forEach(addDisplayImageToPage)
+//     for(displayObject = document.querySelector(".title"))
+//   }
+
+//   function addDisplayImageToPage(displayTitle) {
+//       console.log(displayTitle.title)
+//   }
+
+function addDisplayImage(displayImage) {
+    document.querySelector(".image") = "oldImage"
+    let displayImage = document.createElement('img');
+    for(old )
+
+}
